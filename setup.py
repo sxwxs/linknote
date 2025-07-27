915c6c615f47b8f45878273c89c796e1ea45b053
sxwxs <sxwxs@msn.com>
2025-07-28 00:00:58 +0800

    bump version to 0.1.1;

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="linknote",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
